16d24fafecfb695d9b631ceba6a49e11ab31eb58
ovrmrw <t.ngc.0403@gmail.com>
2016-11-02 00:37:33 +0900

    Update

--- data/firebase-queries.ts
+++ data/firebase-queries.ts
@@ -1,7 +1,7 @@
 import firebase from 'firebase';
 import lodash from 'lodash';
 
-import { User } from './schema';
+import { User, Hobby } from './schema';
 
 
 export function getUserIds(): Promise<string[]> | never {
@@ -10,6 +10,28 @@ export function getUserIds(): Promise<string[]> | never {
       const users: User[] = lodash.toArray<User>(snapshot.val()).filter(obj => !!obj);
       const ids: string[] = users.map(user => user.id ? user.id : '').filter(id => !!id);
       resolve(ids);
+    });
+  });
+}
+
+
+export function getUser(key: string): Promise<User> {
+  return new Promise<User>(resolve => {
+    firebase.database().ref('users/' + key).once('value', snapshot => {
+      const user: User = snapshot.val();
+      console.log('userLoader fetch:', { id: user.id, name: user.name });
+      resolve(user);
+    });
+  });
+}
+
+
+export function getHobby(key: string): Promise<Hobby> {
+  return new Promise<Hobby>(resolve => {
+    firebase.database().ref('hobby/' + key).once('value', snapshot => {
+      const hobby: Hobby = snapshot.val();
+      console.log('hobbyLoader fetch:', { id: hobby.id, name: hobby.name });
+      resolve(hobby);
     });
   });
 }
--- data/loaders.ts
+++ data/loaders.ts
@@ -2,42 +2,33 @@ import firebase from 'firebase';
 const DataLoader = require('dataloader');
 
 import { User, Hobby } from './schema';
+import { getUser, getHobby } from './firebase-queries';
 
 
-export const userLoader: DataLoaderType<string, User>
-  = new DataLoader((keys: string[]) => new Promise<User[]>((resolve, reject) => {
+export const userLoader: DataLoaderType<string, User> = new DataLoader((keys: string[]) => {
+  return new Promise<User[]>((resolve, reject) => {
     const promises: Promise<User>[] | never = keys.map(key => {
-      return new Promise<User>(resolve => {
-        firebase.database().ref('users/' + key).once('value', snapshot => {
-          const user: User = snapshot.val();
-          console.log('userLoader fetch:', { id: user.id, name: user.name });
-          resolve(user);
-        });
-      });
+      return getUser(key);
     });
 
     Promise.all(promises)
       .then(results => resolve(results))
       .catch(err => reject(err));
-  }));
+  });
+});
 
 
-export const hobbyLoader: DataLoaderType<string, Hobby>
-  = new DataLoader((keys: string[]) => new Promise<Hobby[]>((resolve, reject) => {
+export const hobbyLoader: DataLoaderType<string, Hobby> = new DataLoader((keys: string[]) => {
+  return new Promise<Hobby[]>((resolve, reject) => {
     const promises: Promise<Hobby>[] | never = keys.map(key => {
-      return new Promise<Hobby>(resolve => {
-        firebase.database().ref('hobby/' + key).once('value', snapshot => {
-          const hobby: Hobby = snapshot.val();
-          console.log('hobbyLoader fetch:', { id: hobby.id, name: hobby.name });
-          resolve(hobby);
-        });
-      });
+      return getHobby(key);
     });
 
     Promise.all(promises)
       .then(results => resolve(results))
       .catch(err => reject(err));
-  }));
+  });
+});
 
 
 
