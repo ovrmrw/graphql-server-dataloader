752ce4d48c91cc0554f78a1232f9b63a51d3212a
ovrmrw <t.ngc.0403@gmail.com>
2016-11-03 00:44:28 +0900

    Fix

--- deploy.cmd
+++ deploy.cmd
@@ -1,3 +1,8 @@
-robocopy . ../wwwroot /s /XD node_modules
+REM copy files from repository dir to wwwroot dir.
+robocopy . ../wwwroot /S /XD node_modules
+
+REM change current dir to wwwroot.
 cd ../wwwroot
+
+REM run build task.
 npm run build:azure
