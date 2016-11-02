442c42a3eab9ea1fb6f4c5939c9f6b9314aa6716
ovrmrw <t.ngc.0403@gmail.com>
2016-11-03 00:53:22 +0900

    Fix

--- deploy.cmd
+++ deploy.cmd
@@ -1,5 +1,5 @@
 REM copy files from repository dir to wwwroot dir.
-robocopy . ../wwwroot /S /XD node_modules
+robocopy . ../wwwroot /S /XD node_modules jspm_packages .git .vscode
 
 REM change current dir to wwwroot.
 cd ../wwwroot
