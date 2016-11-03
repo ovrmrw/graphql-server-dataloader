ce2659bbc55ec8b2d7c0212f2a98726f0a5aec70
ovrmrw <t.ngc.0403@gmail.com>
2016-11-03 16:29:55 +0900

    Fix

--- data/index.ts
+++ data/index.ts
@@ -2,8 +2,6 @@ import { makeExecutableSchema } from 'graphql-tools';
 
 import { schema } from './schema';
 import { resolverMap } from './resolvers';
-export { createLoaders } from './loaders';
-export { Context } from './types';
 
 
 export const executableSchema = makeExecutableSchema({
