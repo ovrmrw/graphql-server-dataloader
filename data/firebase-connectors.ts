c40827394f42b4fca9094d39f7b39c6df77d0a05
ovrmrw <t.ngc.0403@gmail.com>
2016-11-04 18:02:26 +0900

    Update

--- data/firebase-connectors.ts
+++ data/firebase-connectors.ts
@@ -1,5 +1,6 @@
 import firebase from 'firebase';
 import lodash from 'lodash';
+const assert = require('power-assert');
 
 import { User, Hobby } from './schema';
 
@@ -7,14 +8,17 @@ import { User, Hobby } from './schema';
 export async function getUserIdsConnector(): Promise<string[]> {
   const snapshot = await firebase.database().ref('users').once('value') as Snapshot;
   const users: User[] = lodash.toArray<User>(snapshot.val()).filter(obj => !!obj);
-  const ids: string[] = users.map(user => user.id ? user.id : '').filter(id => !!id);
+  users.forEach(user => assert(lodash.isObject(user) && 'id' in user));
+  const ids: string[] = users.map(user => user.id);
   return ids;
 }
 
 
 export async function getUsersConnector(keys: string[]): Promise<User[]> {
   const snapshotPromises = keys.map(key => firebase.database().ref('users/' + key).once('value') as Promise<Snapshot>);
-  const users: User[] = await Promise.all(snapshotPromises).then(snapshots => snapshots.map(s => s.val()));
+  const snapshots = await Promise.all(snapshotPromises);
+  const users: User[] = snapshots.map(snapshot => snapshot.val());
+  users.forEach(user => assert(lodash.isObject(user) && 'id' in user));
   console.log('userLoader fetch:', ...users.map(user => ({ id: user.id, name: user.name })));
   return users;
 }
@@ -22,7 +26,9 @@ export async function getUsersConnector(keys: string[]): Promise<User[]> {
 
 export async function getHobbiesConnector(keys: string[]): Promise<Hobby[]> {
   const snapshotPromises = keys.map(key => firebase.database().ref('hobby/' + key).once('value') as Promise<Snapshot>);
-  const hobbies: Hobby[] = await Promise.all(snapshotPromises).then(snapshots => snapshots.map(s => s.val()));
+  const snapshots = await Promise.all(snapshotPromises);
+  const hobbies: Hobby[] = snapshots.map(snapshot => snapshot.val());
+  hobbies.forEach(hobby => assert(lodash.isObject(hobby) && 'id' in hobby));
   console.log('hobbyLoader fetch:', ...hobbies.map(hobby => ({ id: hobby.id, name: hobby.name })));
   return hobbies;
 }
